f4b77ea4b8b580edea83c265466e47de42f9d0ba
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-28 15:01:06 -0500

    Small controller tweaks

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model()
+var burger = require("../models/burger.js");
+
+// Create all the routres and set up logic within those routes where required
+
