1ea1a6ee133b1219d9629c1637073c6da6204fce
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-28 12:02:22 -0500

    server and connection code complete

--- server.js
+++ server.js
@@ -17,21 +17,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
-
-
 // Import routes and give the server access to them.
-var routes = require() // FINISH THE CONTROLLER ROUTE
-
-
-
-
+var routes = require("./controllers/burgers_controllers.js")
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
