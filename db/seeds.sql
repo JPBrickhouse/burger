b20d47985ab81b7c4d38e9f96aabf5e6164f3e60
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-28 12:19:52 -0500

    Schema and seeds created and run

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- (Sesame) Seeds for the burgers_db
+
+USE burgers_db;
+
+INSERT into burgers
+    (burger_name)
+VALUES
+    ("Black Bean Veggie Burger"),
+    ("Biggest Mac"),
+    ("Ketchup Only Plain Burger")
