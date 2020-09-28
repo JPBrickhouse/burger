abe5ad97b31b0e550564031073ee86c92b6c1bf1
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-28 12:28:03 -0500

    Initial orm framework

---
+++ config/orm.js
@@ -0,0 +1,45 @@
+// Import MySQL connection
+var connection = require("../config/connection.js");
+
+
+
+
+
+// REFERENCE the CATS APP
+// -- printQuestionMarks function
+// -- objToSql function
+
+
+
+
+
+var orm = {
+
+
+
+
+
+    selectAll: function () {
+
+    },
+
+
+
+
+    insertOne: function () {
+
+    },
+
+
+
+    updateOne: function () {
+
+    }
+
+
+}
+
+
+
+// Export the orm object for the model (burger.js)
+module.exports = orm;
