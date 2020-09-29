00c6f3e2a41975fc013affb15f9040acac93821c
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-29 17:34:59 -0500

    Notes about future functions

---
+++ public/assets/js/eatDaBurger.js
@@ -0,0 +1,16 @@
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+    // Function on click
+    // CHANGE BURGER EATEN STATUS
+    // Making an AJAX call to the API
+
+
+
+
+    // Function on click
+    // ADD A BURGER TO THE LIST
+    // Making an AJAX call to the API
+
+})
